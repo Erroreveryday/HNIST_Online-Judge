c634617e0e4a86183e26be6963cdbb89bce7ab0b
LogicLiu <liujichuan2020@163.com>
2025-01-25 12:19:37 +0800

    1003 字符正方形

---
+++ 1003_字符正方形.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    char inputChar;
+
+    scanf("%c", &inputChar);
+
+
+    printf("%c%c%c\n", inputChar, inputChar, inputChar);
+    printf("%c%c%c\n", inputChar, inputChar, inputChar);
+    printf("%c%c%c\n", inputChar, inputChar, inputChar);
+
+    return 0;
+}
